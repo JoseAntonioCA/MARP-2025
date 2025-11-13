a8251111f0327d3ccde5eca86e4eadc587ad58b3
JoseAntonioCA <jocarm02@ucm.es>
2025-11-13 10:55:51 +0100

    n

---
+++ EjerciciosTema9MARP/MARP9-L/MARP9-L.cpp
@@ -0,0 +1,146 @@
+/*@ <authors>
+ *
+ * MARP11 José Antonio Carmona Alfonsel
+ *
+ *@ </authors> */
+
+
+ /*@ <COSTE>
+  *
+  * El coste es O(N log N) + O(N)
+  *
+  *@ </COSTE> */
+#include <limits>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include "DigrafoValorado.h"
+#include "IndexPQ.h"
+#include <queue>
+using namespace std;
+
+template <typename Valor>
+class Dijkstra {
+    using Camino = std::deque<Valor>;
+public:
+    Dijkstra(DigrafoValorado<Valor> const& g, int orig) : origen(orig),
+        dist(g.V(), INF), ways(g.V(), 0), ulti(g.V()), pq(g.V()) {
+        dist[origen] = 0;
+        ways[origen] = 1;
+        pq.push(origen, 0);
+        while (!pq.empty()) {
+            int v = pq.top().elem; pq.pop();
+            for (auto a : g.ady(v))
+                relajar(a);
+        }
+    }
+    bool hayCamino(int v) const { return dist[v] != INF; }
+    Valor distancia(int v) const { return dist[v]; }
+    Valor caminos(int v) const { return ways[v]; }
+    std::vector<Valor> getDistVector() const {
+        return dist;
+    }
+    //Camino camino(int v) const {
+    //    Camino cam;
+    //    // recuperamos el camino retrocediendo
+    //    AristaDirigida<Valor> a;
+    //    for (a = ulti[v]; a.desde() != origen; a = ulti[a.desde()])
+    //        cam.push_front(a);
+    //    cam.push_front(a);
+    //    return cam;
+    //}
+private:
+    const Valor INF = std::numeric_limits<Valor>::max();
+    int origen;
+    std::vector<Valor> dist;
+    std::vector<Valor> ways;
+    std::vector<AristaDirigida<Valor>> ulti;
+    IndexPQ<Valor> pq;
+    void relajar(AristaDirigida<Valor> a) {
+        int v = a.desde(), w = a.hasta();
+        int distancia = dist[v] + a.valor();
+        if (dist[w] > distancia) {
+            dist[w] = distancia;
+            ways[w] = ways[v];
+            ulti[w] = a;
+            pq.update(w, dist[w]);
+        }
+        else if (dist[w] == distancia) {
+            ways[w] += ways[v];
+        }
+    }
+};
+
+bool resuelveCaso() {
+
+    // leemos la entrada
+    int N, M;
+    cin >> N >> M;
+    if (!cin) return false;
+
+    DigrafoValorado<int> grafo = DigrafoValorado<int>(N);
+
+    int p1, p2;
+    long coste;
+    for (int i = 0; i < M; i++) {
+        cin >> p1 >> p2 >> coste;
+
+        AristaDirigida<int> arista1 = AristaDirigida<int>(p2 - 1, p1 - 1, coste);
+        AristaDirigida<int> arista2 = AristaDirigida<int>(p1 - 1, p2 - 1, coste);
+
+        grafo.ponArista(arista1);
+        grafo.ponArista(arista2);
+    }
+
+    int costeNorte = 0;
+    int costeSur = 0;
+
+    std::vector<bool> visitados = std::vector<bool>(N, false);
+    visitados[0] = true;
+    visitados[N - 1] = true;
+
+    Dijkstra<int> caminosNorte = Dijkstra<int>(grafo, 0);
+    std::vector<int> distanciasNorte = caminosNorte.getDistVector();
+
+    std::sort(distanciasNorte.begin(), distanciasNorte.end());
+
+    Dijkstra<int> caminosSur = Dijkstra<int>(grafo, N-1);
+    std::vector<int> distanciasSur = caminosSur.getDistVector();
+
+    std::sort(distanciasSur.begin(), distanciasSur.end(), greater<int>());
+
+
+    for (int i = 1; i < N/2; i++) {
+        costeNorte += distanciasNorte[i];
+    }
+
+    for (int i = N/2; i < N-1; i++) {
+        costeSur += distanciasSur[i];
+    }
+    
+    cout << costeNorte * 2 + costeSur * 2 << endl;
+
+    return true;
+}
+
+//@ </answer>
+//  Lo que se escriba debajo de esta línea ya no forma parte de la solución.
+
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    if (!in.is_open())
+        cout << "Error: no se ha podido abrir el archivo de entrada." << endl;
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    // Resolvemos
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+#endif
+    return 0;
+}
